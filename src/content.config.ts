a4dda71efe18e32df60c9497c99276247807175e
Maximilian Köhl <koehl@silitics.com>
2026-08-07 06:35:47 +0000

    upgrade documentation site to Astro 7

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,6 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection } from "astro:content";
 import { glob } from "astro/loaders";
+import { z } from "astro/zod";
 import { articleSchema } from "@silitics/astro-theme/content";
 
 const docsSchema = z.object({
